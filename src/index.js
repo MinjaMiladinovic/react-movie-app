19bdd93d8747f0426cd3b4a64c18023ea50edbf8
Adem <adem.huduti@gmail.com>
2019-05-14 12:09:04 +0200

    Setup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import store from './store';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact={true} component={App} />
+      </Switch>
+    </BrowserRouter>
+  </Provider>,
+    document.getElementById('root'));
 
 
 serviceWorker.unregister();
