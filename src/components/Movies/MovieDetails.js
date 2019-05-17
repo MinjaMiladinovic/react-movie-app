8115e3c186b9b90377f29f2a8e716dfc7f7bbb5d
Adem <adem.huduti@gmail.com>
2019-05-17 23:42:34 +0200

    Bug fix

--- src/components/Movies/MovieDetails.js
+++ src/components/Movies/MovieDetails.js
@@ -7,12 +7,16 @@ const MovieDetails = (props) => {
     <div className="col-md-6 col-sm-12">
       <Link to={`/movieDetails/${props.movie.id}`}>
         <div className="text-center mb-3 border border-dark">
-          <img
-            className="img-fluid"
-            src={`https://image.tmdb.org/t/p/original/${props.movie.backdrop_path}`}
-            alt={`${props.movie.title}`}
-          />
-          <h4 className="text-center mt-3">{props.movie.title}</h4>
+          {props.movie.backdrop_path &&
+            <img
+              className="img-fluid"
+              src={`https://image.tmdb.org/t/p/original/${props.movie.backdrop_path}`}
+              alt={`${props.movie.title}`}
+            />
+          }
+          {props.movie.title &&
+            <h4 className="text-center mt-3">{props.movie.title}</h4>
+          }
         </div>
       </Link>
     </div>
